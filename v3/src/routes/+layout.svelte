8d42716f9f2aa648515733cf41fcb33f1b78b1e5
ROBIÑHO <asphalt9fr@gmail.com>
2023-10-10 09:31:14 +0200

    fix : redirect on layout button

--- v3/src/routes/+layout.svelte
+++ v3/src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import "../app.css";
+    import { goto } from "$app/navigation";
 
     import "nprogress/nprogress.css";
     import NProgress from "nprogress";
@@ -18,10 +19,13 @@
 </script>
 
 <header class="bg-white">
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
     <div
       class="mx-auto flex h-16 max-w-screen-xl items-center gap-8 px-4 sm:px-6 lg:px-8"
     >
-    <img src="https://newton-creations.fr/favicon.ico" alt="logo" class="h-8 w-8">
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
+    <img src="https://newton-creations.fr/favicon.ico" alt="logo" class="h-8 w-8" on:click={()=>goto("/")}>
   
         <div class="flex flex-1 items-center justify-end md:justify-between">
           <ul class="flex items-center gap-6 text-sm">
