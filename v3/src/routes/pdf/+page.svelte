d45e53f6812ae65147bb17ebd980dc8a37b69b85
newtondotcom <asphalt9fr@gmail.com>
2023-12-05 11:43:00 +0100

    father for pdf component

---
+++ v3/src/routes/pdf/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import Pdf from "$lib/Pdf.svelte";
+    import { onMount } from "svelte";
+
+    var path:any;
+    let displaypdf:boolean = false;
+
+    onMount(()=> {
+        path = sessionStorage.getItem("pdfURI");
+        displaypdf = true;
+    });
+</script>
+
+{#if displaypdf}
+<Pdf blob={path}/>
+{/if}
