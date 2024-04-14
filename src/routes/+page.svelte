7cd271a9ce70db0b414079a28fde8ae67f6e09e2
newtondotcom <asphalt9fr@gmail.com>
2024-04-14 23:53:56 +0200

    placeholder for addressé

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -97,7 +97,7 @@
   <label
   class="block mt-4 sm:w-3/4 xl:w-2/5 overflow-hidden rounded-md border border-gray-200 px-3 py-2 shadow-sm focus-within:border-emerald-600 focus-within:ring-1 focus-within:ring-emerald-600"
   >
-    <div class="w-64 text-base font-medium text-gray-700">Téléphone</div>
+    <div class="w-64 text-base font-medium text-gray-700">Adresse mail</div>
     <input
       type="tel"
       bind:value={mail}
